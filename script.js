30561279650b62bb7b448a242eccb0594c62b4b3
fazza-tech <ebranfazza@gmail.com>
2024-10-28 08:21:56 +0530

    updated js

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnShowModal = document.querySelectorAll('.show-modal');
+
+for (let i =0; i< btnShowModal.length; i++){
+    btnShowModal[i].addEventListener('click' , function(){
+        modal.classList.remove('hidden');
+        overlay.classList.remove('hidden');
+    })
+}
+
+const closeModal = () => {
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
+}
+
+btnCloseModal.addEventListener('click' , closeModal)
+
+overlay.addEventListener('click' , closeModal) //why we didnt cal the function like closemodal() ?? bcz if we write like that, the js will excecute it immediatly when our live code run.
+
+
